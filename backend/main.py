745e538a5735151da1195698574cf67a31b39f10
Aaveg-Shang <91478078+Aaveg-Shang@users.noreply.github.com>
2023-09-16 18:01:03 -0400

    Started using a customtkinter gui

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,12 @@
-import customtkinter
+import customtkinter as ctk
+
+# Create the Custom Tkinter Window
+ctk.set_appearance_mode("dark")
+root = ctk.CTk()
+root.geometry("1920x1080")
+
+# Create the sidebar housing main functionalities
+sidebar = ctk.CTkFrame(master=root, width=610, height=1080, border_width=1, border_color='white')
+sidebar.place(relx=0.125, rely=0.5, anchor='center')
+
+root.mainloop()
