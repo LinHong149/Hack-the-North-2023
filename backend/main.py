8661439efba1b35d9f5e981e69974aad4e967e74
LinHong <linlin.hong1@gmail.com>
2023-09-16 19:19:45 -0400

    customtkinter

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from tkinter import PhotoImage
 import customtkinter as ctk
 
 # Create the Custom Tkinter Window
@@ -5,8 +6,45 @@ ctk.set_appearance_mode("dark")
 root = ctk.CTk()
 root.geometry("1920x1080")
 
+showPage = "home"
+def nav1_clicked():
+    showPage = "home"
+def nav2_clicked():
+    showPage = "insight"
+def nav3_clicked():
+    showPage = "my_vision"
+
 # Create the sidebar housing main functionalities
 sidebar = ctk.CTkFrame(master=root, width=610, height=1080, border_width=1, border_color='white')
 sidebar.place(relx=0.125, rely=0.5, anchor='center')
+
+# Nav Logo
+logo = PhotoImage(file="./public/logo192.png")
+# logo = logo.resize((10))
+nav_logo = ctk.CTkButton(master=root, width=400, height=50, image=logo, hover=False, text=None, command=nav1_clicked)
+nav_logo.place(relx=0.1666, rely=0.1, anchor='center')
+
+# Nav Buttons
+nav_button1 = ctk.CTkButton(master=root, width=400, height=50, text="Home", command=nav1_clicked)
+nav_button1.place(relx=0.1666, rely=0.18, anchor='center')
+nav_button2 = ctk.CTkButton(master=root, width=400, height=50, text="insight", command=nav2_clicked)
+nav_button2.place(relx=0.1666, rely=0.26, anchor='center')
+nav_button3 = ctk.CTkButton(master=root, width=400, height=50, text="My Vision", command=nav3_clicked)
+nav_button3.place(relx=0.1666, rely=0.34, anchor='center')
+
+
+if showPage == "home":
+    #show home
+    test = ctk.CTkLabel(master=root, text="home")
+    test.place(relx=0.667, rely=0.5, anchor='center')
+elif showPage == "insight":
+    #show second page
+    test = ctk.CTkLabel(master=root, text="insight")
+    test.place(relx=0.667, rely=0.5, anchor='center')
+elif showPage == "my_vision":
+    #show vision page
+    test = ctk.CTkLabel(master=root, text="my vision")
+    test.place(relx=0.667, rely=0.5, anchor='center')
+
 
 root.mainloop()
